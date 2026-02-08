8502c00a160d4f1d431aa8f6b0b0f3e7d06fef1b
Fuadwa <fuadjemal35@gmail.com>
2026-02-08 17:41:25 +0300

    firsgt commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders react text', () => {
-  render(<App />);
-  const textElement = screen.getByText(/react/i);
-  expect(textElement).toBeInTheDocument();
-});
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
 test('renders hello world text', () => {
   render(<App />);
-  const textElement = screen.getByText(/hellow world/i);
+  const textElement = screen.getByText(/hellow world/i); // match the text in App.js
   expect(textElement).toBeInTheDocument();
 });
